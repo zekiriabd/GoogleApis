c1569bf5ee51b6a3339a73506a49e68e22393ff2
zekir <zekiriabd@hotmail.com>
2024-10-09 21:15:50 +0200

    test

---
+++ CloudRunDemo/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudRunDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet(Name = "About")]
+        public string Get()
+        {
+            return "Like and subscribe!!!";
+        }
+    }
+}
